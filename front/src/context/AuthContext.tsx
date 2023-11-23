3252b6f3226bd60750c29026fe63053ce47261e7
Blais Mathieu <mathieu.blais@epitech.eu>
2023-11-23 14:10:28 +0100

    [FIX] fixed some little mistakes on the login

--- front/src/context/AuthContext.tsx
+++ front/src/context/AuthContext.tsx
@@ -1,6 +1,12 @@
 // src/context/AuthContext.tsx
-import React, { createContext, useContext, useState, ReactNode, useCallback } from 'react';
-import { apiClient } from '../services/api';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useCallback,
+} from "react";
+import { apiClient } from "../services/api";
 
 interface AuthContextType {
   user: any;
@@ -8,6 +14,7 @@ interface AuthContextType {
   logout: () => Promise<void>;
   getLoginStatus: () => Promise<void>;
   register: (email: string, password: string, name: string) => Promise<void>; // Add the register method
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextType>(null!);
@@ -18,37 +25,47 @@ interface AuthProviderProps {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const login = async (email: string, password: string) => {
     const response = await apiClient.login(email, password);
-    setUser(response.data.user);
+    setUser(response.data);
+    setLoading(false);
   };
 
   const logout = async () => {
     await apiClient.logout();
     setUser(null);
+    setLoading(true);
   };
 
   const register = async (email: string, password: string, name: string) => {
     const response = await apiClient.register(email, password, name);
     setUser(response.data.user);
+    setLoading(true);
   };
 
   const getLoginStatus = useCallback(async () => {
     try {
+      setLoading(true);
       const response = await apiClient.getLoginStatus();
       if (response.data && response.data.userInfo) {
+        setLoading(false);
         setUser(response.data.userInfo); // Set the user state with the userInfo data
       } else {
-        console.error('User data is undefined', response.data);
+        setLoading(false);
+        console.error("User data is undefined", response.data);
       }
     } catch (error) {
-      console.error('Error fetching login status:', error);
+      setLoading(false);
+      console.error("Error fetching login status:", error);
     }
   }, [setUser]);
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, register, getLoginStatus }}>
+    <AuthContext.Provider
+      value={{ user, login, logout, register, getLoginStatus, loading }}
+    >
       {children}
     </AuthContext.Provider>
   );
@@ -57,7 +74,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
