defd989717f2abc79be4fd19d6ed6da52c9a828b
Blais Mathieu <mathieu.blais@epitech.eu>
2024-04-18 15:24:36 +0200

    [ADD] added fake notifications

--- front/src/pages/notifications.tsx
+++ front/src/pages/notifications.tsx
@@ -1,3 +1,67 @@
+import { InfoEmpty, WarningHexagon, WarningTriangle } from "iconoir-react";
+import { useAuth } from "../context/AuthContext";
+
+function NotificationError(props: { title: string; description: string }) {
+  return (
+    <div className="bg-red-300/80 text-red-800 flex justify-between py-3 px-5 rounded-lg items-start">
+      <div className="flex gap-6 items-center">
+        <WarningTriangle width={35} height={35} strokeWidth={2} />
+        <div className="flex flex-col">
+          <p className="font-semibold">{props.title}</p>
+          <p className="text-sm">{props.description}</p>
+        </div>
+      </div>
+      <p className="text-sm underline">21 March, 11h30</p>
+    </div>
+  );
+}
+
+function NotificationInfo(props: { title: string; description: string }) {
+  return (
+    <div className="bg-indigo-400 text-indigo-800 flex justify-between py-3 px-5 rounded-lg items-start">
+      <div className="flex gap-6 items-center">
+        <InfoEmpty width={32} height={32} strokeWidth={2} />
+        <div className="flex flex-col">
+          <p className="font-semibold">{props.title}</p>
+          <p className="text-sm">{props.description}</p>
+        </div>
+      </div>
+      <p className="text-sm underline">Today, 13h30</p>
+    </div>
+  );
+}
+
 export default function NotificationsPage() {
-  return <div>{/* <p>notifications</p> */}</div>;
+  const { user } = useAuth();
+
+  if (!user.notified)
+    return (
+      <div className="mt-6 px-4 sm:px-6 lg:px-8 flex flex-col gap">
+        <div className="bg-Light-gray dark:bg-[#E5E5E5] rounded-lg shadow-lg p-6">
+          <p className="text-lg font-semibold text-white">Notifications</p>
+          <p className="mt-4 text-white/80">Your notifications are disabled</p>
+        </div>
+      </div>
+    );
+  return (
+    <div className="mt-6 px-4 sm:px-6 lg:px-8 flex flex-col gap">
+      <div className="bg-Light-gray dark:bg-[#E5E5E5] rounded-lg shadow-lg p-6">
+        <p className="text-lg font-semibold text-white">Notifications</p>
+        <div className="flex flex-col gap-5 mt-5">
+          <NotificationError
+            title={"Baisse anormale de température"}
+            description="La température de votre ruche 'Hive de demo' a baissé de 8 degrès durant les 5 dernières heures."
+          />
+          <NotificationError
+            title={"Poids de la ruche élevé"}
+            description="Le poids de votre ruche 'Hive de demo' est de 22kg, vous devriez vous en occuper."
+          />
+          <NotificationInfo
+            title={"Variations de températures exterieures importantes"}
+            description="Des phénomènes météorologiques entrainant de fortes variation de températures vont se produire prochainement."
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- front/src/pages/profile.tsx
+++ front/src/pages/profile.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {  useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import { User } from "iconoir-react";
 import axios from "axios";
 import { API_BASE_URL } from "../services/api";
@@ -9,6 +9,21 @@ const ProfilePage = () => {
   const [notificationsEnabled, setNotificationsEnabled] = useState(false);
   const { user } = useAuth();
   const { pushSnackbar } = useSnackbarsContext();
+  const [theme, setTheme] = useState(() => {
+    // Check for the theme in localStorage
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? savedTheme : "light";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   const toggleNotifications = async () => {
     setNotificationsEnabled(!notificationsEnabled);
@@ -29,6 +44,10 @@ const ProfilePage = () => {
     if (user) setNotificationsEnabled(user.notified);
   }, [user]);
 
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <div className="min-h-screen bg-main dark:bg-[#F5F5F5] flex justify-center items-start py-12 px-4 sm:px-6 lg:px-8">
       <div className=" w-[70%] space-y-8 ">
@@ -44,7 +63,7 @@ const ProfilePage = () => {
               {user.email}
             </p>
           </div>
-          <div className="mt-6 border-t border-gray-200 dark:border-gray-300 pt-4">
+          <div className="mt-6 border-t border-gray-200 dark:border-gray-500 pt-4">
             <h2 className="text-lg font-semibold text-white dark:text-black">
               {" "}
               Notifications settings
@@ -64,6 +83,31 @@ const ProfilePage = () => {
                   aria-hidden="true"
                   className={`${
                     notificationsEnabled ? "translate-x-5" : "translate-x-0"
+                  } inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
+                ></span>
+              </button>
+            </div>
+          </div>
+          <div className="mt-6 border-t border-gray-200 dark:border-gray-300 pt-4">
+            <h2 className="text-lg font-semibold text-white dark:text-black">
+              {" "}
+              Color Theme
+            </h2>
+            <div className="mt-4 flex items-center justify-between">
+              <span className="text-sm text-white dark:text-black">
+                Enable Dark theme
+              </span>
+              <button
+                aria-pressed="false"
+                className={`relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+                  theme === "light" ? "bg-greenOlive" : "bg-gray-200"
+                }`}
+                onClick={() => handleThemeSwitch()}
+              >
+                <span
+                  aria-hidden="true"
+                  className={`${
+                    theme === "light" ? "translate-x-5" : "translate-x-0"
                   } inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
                 ></span>
               </button>
