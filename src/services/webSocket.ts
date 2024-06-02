7b472189b1d367bb06492b0ab5c2bb3a226dc937
Clément L <clement.loiseau@epitech.eu>
2024-06-03 01:47:57 +0200

    plz work

--- src/services/webSocket.ts
+++ src/services/webSocket.ts
@@ -1,23 +1,24 @@
-const WS_URL= import.meta.env.VITE_APP_WS;
+// const WS_URL= import.meta.env.VITE_APP_WS;
+//
+// console.log("WS_URL:", WS_URL);
+//
+// const ws = new WebSocket(WS_URL);
+//
+// ws.onopen = () => {
+//   console.log('Connected to the WebSocket server');
+// };
+//
+// ws.onclose = () => {
+//   console.log('Disconnected from the WebSocket server');
+// };
+//
+// ws.onerror = (error) => {
+//   console.error('WebSocket Error:', error);
+// };
+//
+// ws.onmessage = (event) => {
+//   console.log('Message from server:', event.data);
+// };
+//
+// export default ws;
 
-console.log("WS_URL:", WS_URL);
-
-const ws = new WebSocket(WS_URL);
-
-ws.onopen = () => {
-  console.log('Connected to the WebSocket server');
-};
-
-ws.onclose = () => {
-  console.log('Disconnected from the WebSocket server');
-};
-
-ws.onerror = (error) => {
-  console.error('WebSocket Error:', error);
-};
-
-ws.onmessage = (event) => {
-  console.log('Message from server:', event.data);
-};
-
-export default ws;
